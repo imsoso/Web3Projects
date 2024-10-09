2a2b44d092ebc5f09b886e6da283c599f64f9f8e
Soso <husosong@163.com>
2024-10-09 16:59:26 +0800

    Feature: Add string has 4 and 5 zero function and Unit Test

--- hash_zero/src/main.rs
+++ hash_zero/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use regex::Regex;
+use sha256::digest;
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let five_zero = "000abc00";
+        let has_5_zeros = check_string_has_5_zeros(five_zero);
+        assert_eq!(has_5_zeros, true);
+
+        let four_zero = "00abc00";
+        let has_4_zeros = check_string_has_4_zeros(four_zero);
+        assert_eq!(has_4_zeros, true);
+    }
+}
+
+fn check_string_has_5_zeros(input_string: &str) -> bool {
+    let re = Regex::new(r"^(?:[^0]*0){5}[^0]*$").unwrap();
+    re.is_match(input_string)
+}
+
+fn check_string_has_4_zeros(input_string: &str) -> bool {
+    let re = Regex::new(r"^(?:[^0]*0){4}[^0]*$").unwrap();
+    re.is_match(input_string)
 }
