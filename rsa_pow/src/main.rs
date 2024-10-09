2a15e8b0e70ea78c563fbf34d4f2cb96b5e8d868
Soso <husosong@163.com>
2024-10-09 17:56:15 +0800

    Feature：Generate public and private key using RSA

---
+++ rsa_pow/src/main.rs
@@ -0,0 +1,8 @@
+use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let bits = 2048;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let pub_key = RsaPublicKey::from(&priv_key);
+}
