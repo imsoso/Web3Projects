83702c89502027f323d211566a5566f8a5112937
Soso <husosong@163.com>
2024-10-11 16:49:55 +0800

    Feature: Add update and get balance function

--- solidityWorkspace/Bank.sol
+++ solidityWorkspace/Bank.sol
@@ -3,8 +3,17 @@ pragma solidity >=0.8.0;
 
 contract Bank {
     mapping(address => uint) public deposits;
+    mapping(address => uint) public balances;
 
     function deposit() public payable {
         deposits[msg.sender] += msg.value;
+    }
+
+    function update(uint newBalance) public {
+        balances[msg.sender] = newBalance;
+    }
+
+    function get(address key) public view returns (uint) {
+        return balances[key];
     }
 }
