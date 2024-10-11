56d37e54b8d5797dfae488b31760643034cc68e2
Soso <husosong@163.com>
2024-10-11 17:36:02 +0800

    improvement: Add code comments

--- solidityWorkspace/Bank.sol
+++ solidityWorkspace/Bank.sol
@@ -1,4 +1,16 @@
 //SPDX-License-Identifier: MIT
+/*
+• 通过 Metamask 向Bank合约存款（转账ETH）
+• 在Bank合约记录每个地址存款⾦额
+• ⽤数组记录存款⾦额前 3 名
+• 编写 Bank合约 withdraw(), 实现只有管理员提取出所有的 ETH
+
+• 理解合约作为⼀个账号、也可以持有资产
+• msg.value / 如何传递 Value
+• 回调函数的理解（receive/fallback）
+• Payable 关键字理解
+• Mapping 、数组使⽤
+*/
 pragma solidity >=0.8.0;
 
 contract Bank {
