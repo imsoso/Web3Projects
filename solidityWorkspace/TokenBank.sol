3b0c6b6845e967c22ae56fb9a9050a0e82f94ff6
Soso <husosong@163.com>
2024-10-14 20:02:10 +0800

    Feature:Init tokenBank

---
+++ solidityWorkspace/TokenBank.sol
@@ -0,0 +1,10 @@
+#// SPDX-License-Identifier: MIT
+#pragma solidity version 0.8.0;
+
+contract TokenBank is {
+    deposit(address, uint256) public {
+
+    }
+    withdraw(address, uint256) public {}
+
+}
