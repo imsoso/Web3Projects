5535315f5b86510cc5d5e9faf6dea211c707f35a
Soso <husosong@163.com>
2024-10-15 21:11:32 +0800

    Feature:Init NFT

--- solidityWorkspace/NFTTokenShopping.sol
+++ solidityWorkspace/NFTTokenShopping.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 /*用 ERC721 标准（可复用 OpenZepplin 库）发行一个自己 NFT 合约，并用图片铸造几个 NFT ， 请把图片和 Meta Json数据上传到去中心的存储服务中，请贴出在 OpenSea 的 NFT 链接。
-*/
+ */
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SosoNFT is ERC721 {
+    constructor() public ERC721("SosoNFT", "SSFT") {}
+}
